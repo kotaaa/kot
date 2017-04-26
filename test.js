8c0df09d674fb3e9b5baf38b139d6be8efa2b052
KOT <viplord219@gmail.com>
2017-04-27 00:06:56 +0500

    123
    Hellow World!!!

---
+++ test.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+		<meta charset="utf-8">
+  <title>getElementById example</title>
+  <script>
+	function Print10(n)
+	{
+	    document.write(n+"<br>");
+	    if (n<11) Print10(n+1);
+	}
+	Print10(1);
+  </script>
+</head>
+<body>
+lalala
+</body>
+</html>
